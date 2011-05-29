c7ac6f4a7d5fcf52be01715941df73973ea98286
Marco Barbosa <iam@marcobarbosa.com>
2011-05-29 20:09:44 +0200

    molajo.builder class

---
+++ js/mylibs/molajo.builder.js
@@ -0,0 +1,13 @@
+var MolajoBuilder = new Class({
+
+    Implements: [Options],
+
+    options: {},
+
+    initialize: function(options, elements) {
+        // Set options
+        this.setOptions(options);
+    }
+    
+
+});
