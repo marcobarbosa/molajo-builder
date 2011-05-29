d44e736dc238faf2d605c6005ae7b89dc672ed38
Marco Barbosa <iam@marcobarbosa.com>
2011-05-29 20:56:42 +0200

    added model to take care of JSON requests

---
+++ js/mylibs/molajo.builder.model.js
@@ -0,0 +1,15 @@
+var MolajoBuilderModel = new Class({
+
+    Implements: [Options],
+
+    options: {},
+
+    availablePackages: {},
+
+    initialize: function(options, elements) {
+        // Set options
+        this.setOptions(options);
+    }
+    
+
+});
